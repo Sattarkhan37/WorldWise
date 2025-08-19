49efc451df1bc65f15f61067912303649d66e4a5
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-19 19:14:53 +0530

    bug fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,15 @@
-import Styles from "./Sidebar.module.css";
+import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import { Outlet } from "react-router-dom";
 function Sidebar() {
   return (
-    <div className={Styles.sidebar}>
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
       <Outlet/>
-      <footer className={Styles.footer}>
-        <p className={Styles.copyright}>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
         </p>
       </footer>
