526a6a0a067f034b15914b940e6479f065c4d7a6
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-20 02:00:16 +0530

    Dynamic Routes With URL parameters

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>
+        position:{lat},{lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 44 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
