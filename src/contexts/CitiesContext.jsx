ab271774cb3389cbc26af2d68ecb7719e8dc38e5
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-22 03:08:33 +0530

    feat: consume CitiesContext in components

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 const BASE_URL = "http://localhost:8000";
 const CitiesContext = createContext();
 function CitiesProvider({ children }) {
@@ -30,4 +30,9 @@ function CitiesProvider({ children }) {
     </CitiesContext.Provider>
   );
 }
-export {CitiesProvider}
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
+export { CitiesProvider ,useCities};
